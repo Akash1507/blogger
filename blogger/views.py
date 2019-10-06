30de5e4d8fbeed3188a2642d44922511183037d4
Akash1507 <atakashsharma@gmail.com>
2019-10-06 16:32:28 +0530

    pagination facility added

--- blogger/views.py
+++ blogger/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404, redirect
@@ -7,7 +6,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .forms import PostModelForm
 from .models import Post
 
-@staff_member_required
+
 def createPost(request):
     form = PostModelForm(request.POST or None, request.FILES or None)
     if form.is_valid():
@@ -19,7 +18,7 @@ def createPost(request):
     context = {'form': form}
     return render(request, template_name, context)
 
-@staff_member_required
+
 def updatePost(request, postUrl):
     obj = get_object_or_404(Post, postUrl=postUrl)
     form = PostModelForm(request.POST or None, instance=obj)
@@ -30,7 +29,7 @@ def updatePost(request, postUrl):
     return render(request, template_name, context)
 
 
-@staff_member_required
+
 def deletePost(request, postUrl):
     obj = get_object_or_404(Post, postUrl=postUrl)
     template_name = 'delete.html'
@@ -40,14 +39,35 @@ def deletePost(request, postUrl):
     context = {"object": obj}
     return render(request, template_name, context)
 
+def make_pagination_html(current_page, total_pages):
+    pagination_string = ""
+    if current_page > 1:
+        pagination_string += '<li><a href="?page=%s">◀</a></li>' % (current_page -1)
+    pagination_string += '<li><span class="current"> Page %s of %s </span></li>' %(current_page, total_pages)
+    if current_page < total_pages:
+        pagination_string += '<li><a href="?page=%s">▶</a></li>' % (current_page + 1)
+    return pagination_string
+
 
 def listPost(request):
     postObjects = Post.objects.all().published()
     if request.user.is_authenticated:
-        data = Post.objects.all()
-        postData = (postObjects | data).distinct()
-    template_name = 'list.html'
-    context = {'objectList': postData}
+        print(request.user)
+        template_name = 'list.html'
+        current_page = int(request.GET.get('page', '1'))
+        limit = int(5 * current_page)
+        offset = limit - 5
+        post_list = Post.objects.filter(postUser=request.user)[offset:limit]
+        total_posts = Post.objects.all().count()
+        total_pages = total_posts // 5
+        if total_posts % 5 != 0:
+            total_pages += 1
+        pagination = make_pagination_html(current_page, total_pages)
+        context = {'objectList': post_list, 'pagination': pagination}
+
+    else:
+        template_name = 'normal.html'
+        context = {}
     return render(request, template_name, context)
 
 
